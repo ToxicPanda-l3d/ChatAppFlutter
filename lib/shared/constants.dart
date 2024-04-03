117e27cfe048ead57ddd5ee565010f61312d9e21
Jainam Jain <132193726+ToxicPanda-l3d@users.noreply.github.com>
2024-04-03 17:36:58 +0530

    Add files via upload

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:300013921284:web:825e39dc0a39f7c798c151";
+  static String apiKey = "AIzaSyAP7ND9TjffmMz1hDF_EvkesEiZm_PTco8";
+  static String messagingSenderId = "300013921284";
+  static String projectId = "chatappflutter-a26b2";
+  final primaryColor = const Color(0xFFee7b64);
+}
